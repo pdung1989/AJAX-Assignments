cdd2ab210fd76c8f6a51dca4829db5ecc578d686
Dung Tran <pdung1989@gmail.com>
2021-11-18 10:39:34 +0200

    mouse trail assignment

---
+++ events/mousetrail/mousetrail.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// create an array of dot
+const elements = [];
+for (let i = 0; i < 15; i++) {
+  let dot = document.createElement('div');
+  dot.className = 'trail';
+  document.body.appendChild(dot);
+  elements.push(dot);
+}
+
+//display trails
+var currentDot = 0;
+function displayTrail(event) {
+  var dot = elements[currentDot];
+  dot.style.left = event.pageX - 4 + 'px';
+  dot.style.top = event.pageY - 4 + 'px';
+  if (currentDot == 14) {
+    currentDot = 0;
+  } else currentDot++;
+}
+
+window.addEventListener('mousemove', displayTrail);
